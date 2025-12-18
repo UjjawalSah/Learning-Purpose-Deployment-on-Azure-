7d9792c55f0d64bd13fe5f183853b5881b132de9
👨‍💻 Ujjawal Kumar <116669610+UjjawalSah@users.noreply.github.com>
2025-12-18 20:32:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import os
-from flask import Flask, jsonify
 
-# Create a Flask application instance
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
 app = Flask(__name__)
 
-# Define a route for the root URL
-@app.route('/')
-def home():
-    return jsonify(message="Hello, Azure!")
 
-# Run the application
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
-    app.run(debug=debug)
+   app.run()
